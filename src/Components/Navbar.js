a2a045c77d88a93e0e7f5eb319c336c33e647a06
rksnyder7 <rksnyder7@gmail.com>
2018-07-24 12:47:46 -0500

    fix navbar collapse

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -16,7 +16,7 @@ const NavbarFunc = ({ onItemClick }) => {
 		<div className='navbarText'>
 		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"/>
 
-			<Navbar className='navbar-fixed-top navbar-inverse'>
+			<Navbar collapseOnSelect className='navbar-fixed-top navbar-inverse'>
 	          <Navbar.Header>
 	              <Navbar.Brand>
 	                <span>Fantasy Football</span>
@@ -24,7 +24,7 @@ const NavbarFunc = ({ onItemClick }) => {
 	              <Navbar.Toggle/>
 	          </Navbar.Header>
 	          <Navbar.Collapse>
-		          <Nav>
+		      <Nav>
 		          	<NavItem id="navbarHome" onClick={()=>onItemClick(2)}>
 		          		<span>Home</span>
 		          	</NavItem>
@@ -34,7 +34,7 @@ const NavbarFunc = ({ onItemClick }) => {
 		           	<NavDropdown title="Standard" id="basic-nav-dropdown">
 		           		{navLinks[1]}
 		            </NavDropdown>
-		          </Nav>
+	          </Nav>
            	  <Navbar.Text pullRight>Draft Strategy Guide</Navbar.Text>
 	          </Navbar.Collapse>
 	        </Navbar>
